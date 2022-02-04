b41b6f93f4ea7f15f9770ab3b735f25733fea4e9
Victor Batista <vitinho7620@gmail.com>
2022-02-04 11:57:18 -0300

    adicionando vite pra otimizar o desenvolvimento

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Content } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <h1 style={{ color: "white" }}>Template</h1>
+        <button type="button">Hello World</button>
+      </Content>
+    </Container>
+  );
+}
